e272526d3f67c5a3e841102a2bc7f6ba63bc0d71
vian151004 <vianmaulana540@gmail.com>
2025-03-19 13:26:39 +0700

    update folder

---
+++ jobsheet 2/geometri.py
@@ -0,0 +1,17 @@
+# Modul geometri.py berisi fungsi terkait geometri
+
+def hitung_luas_persegi(sisi):
+    """Menghitung luas persegi."""
+    return sisi * sisi
+
+def hitung_luas_persegi_panjang(panjang, lebar):
+    """Menghitung luas persegi panjang."""
+    return panjang * lebar
+
+def hitung_luas_lingkaran(jari_jari):
+    """Menghitung luas lingkaran."""
+    return 3.14 * (jari_jari ** 2)
+
+def hello_pray():
+    """Contoh fungsi untuk menyapa."""
+    return "Echo hallo pray"
